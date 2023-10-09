8ca7109fcae798485cf654ae9ad6472f10e46579
3bch <346956+3bch@users.noreply.github.com>
2023-10-09 20:56:56 +0900

    issue #35 icon の padding を 0 に設定

--- pwa-assets.config.js
+++ pwa-assets.config.js
@@ -1,6 +1,16 @@
 import { defineConfig, minimalPreset as preset } from '@vite-pwa/assets-generator/config';
 
 export default defineConfig({
-  preset,
+  preset: {
+    ...preset,
+    maskable: {
+      sizes: [512],
+      padding: 0,
+    },
+    apple: {
+      sizes: [180],
+      padding: 0,
+    },
+  },
   images: ['public/icon.svg'],
 });
