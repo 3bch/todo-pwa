0bfffda2f297285342725d815c26c13e14a608d2
3bch <346956+3bch@users.noreply.github.com>
2023-10-29 11:09:35 +0900

    issue #4 予定を常に Redis に保存するようにした

---
+++ api/backup.mjs
@@ -0,0 +1,31 @@
+import { Redis } from '@upstash/redis';
+import { safeParse } from 'valibot';
+
+import { BackupSchema } from './_schema.mjs';
+
+// 期限は 5 日
+const BACKUP_EXPIRE = 60 * 60 * 24 * 5;
+
+const kv = new Redis({
+  url: process.env.UPSTASH_REDIS_REST_URL,
+  token: process.env.UPSTASH_REDIS_REST_TOKEN,
+});
+
+export default async function handler(request, response) {
+  const body = request.body;
+  const parsed = safeParse(BackupSchema, body);
+
+  if (!parsed.success) {
+    response.status(400).json(parsed.error);
+    return;
+  }
+
+  const { endpoint, schedules } = parsed.data;
+  console.log(`backup ${endpoint}: [${schedules.length}] schedules`);
+
+  await kv.set(`backup:${endpoint}`, schedules, {
+    ex: BACKUP_EXPIRE,
+  });
+
+  response.status(201).send('');
+}
